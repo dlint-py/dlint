5e7243b3ac4738301ac7e7498318c891db0ccdff
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2020-10-14 22:37:32 +0200

    expand Subprocess testing
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- tests/test_bad_subprocess_use.py
+++ tests/test_bad_subprocess_use.py
@@ -19,11 +19,14 @@ class TestBadSubprocessUse(dlint.test.base.BaseTest):
             """
             import subprocess
 
+            command = "/bin/echo"
+
             subprocess.call(shell=True)
             subprocess.check_call(shell=True)
             subprocess.check_output(shell=True)
             subprocess.Popen(shell=True)
             subprocess.run(shell=True)
+            subprocess.run(command,shell=True)
             """
         )
 
@@ -32,16 +35,6 @@ class TestBadSubprocessUse(dlint.test.base.BaseTest):
 
         result = linter.get_results()
         expected = [
-            dlint.linters.base.Flake8Result(
-                lineno=4,
-                col_offset=0,
-                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
-            ),
-            dlint.linters.base.Flake8Result(
-                lineno=5,
-                col_offset=0,
-                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
-            ),
             dlint.linters.base.Flake8Result(
                 lineno=6,
                 col_offset=0,
@@ -57,9 +50,85 @@ class TestBadSubprocessUse(dlint.test.base.BaseTest):
                 col_offset=0,
                 message=dlint.linters.BadSubprocessUseLinter._error_tmpl
             ),
+            dlint.linters.base.Flake8Result(
+                lineno=9,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
+            dlint.linters.base.Flake8Result(
+                lineno=10,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
+            dlint.linters.base.Flake8Result(
+                lineno=11,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
         ]
 
         assert result == expected
+
+    def test_subprocess_usage(self):
+        python_node = self.get_ast_node(
+            """
+            import subprocess
+
+            command = "/bin/echo"
+
+            subprocess.call(command)
+            subprocess.call(shell=False)
+            subprocess.check_call(shell=False)
+            subprocess.check_output(shell=False)
+            subprocess.Popen(shell=False)
+            subprocess.run(command)
+            subprocess.run(shell=False)
+            """
+        )
+
+        linter = dlint.linters.BadSubprocessUseLinter()
+        linter.visit(python_node)
+
+        result = linter.get_results()
+        expected = [
+            dlint.linters.base.Flake8Result(
+                lineno=6,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
+            dlint.linters.base.Flake8Result(
+                lineno=7,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
+            dlint.linters.base.Flake8Result(
+                lineno=8,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
+            dlint.linters.base.Flake8Result(
+                lineno=9,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
+            dlint.linters.base.Flake8Result(
+                lineno=10,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
+            dlint.linters.base.Flake8Result(
+                lineno=11,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
+            dlint.linters.base.Flake8Result(
+                lineno=12,
+                col_offset=0,
+                message=dlint.linters.BadSubprocessUseLinter._error_tmpl
+            ),
+        ]
+
+        assert result != expected
 
 
 if __name__ == "__main__":
