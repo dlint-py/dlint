9e4b15deba5194d63e607d65d6a4b4b2d43265cc
clavedeluna <danalitovsky+git@gmail.com>
2022-09-07 16:22:35 -0300

    remove unused util code

--- dlint/util.py
+++ dlint/util.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python
 
 import abc
-import sys
 
-if sys.version_info >= (3, 4):
-    ABC = abc.ABC
-else:
-    ABC = abc.ABCMeta(str('ABC'), (), {})
+ABC = abc.ABC
 
 
 def lstartswith(l1, l2):
     if len(l2) > len(l1):
         return False
     return l1[:len(l2)] == l2
-
-
-def lendswith(l1, l2):
-    if len(l2) > len(l1):
-        return False
-    return l1[len(l1) - len(l2):] == l2
