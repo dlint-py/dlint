ac3118d84b5622be9b8f96aa376db5cdd80db7a5
clavedeluna <danalitovsky+git@gmail.com>
2022-09-11 16:08:45 -0300

    keep lenswith util function for strategic reasons

--- dlint/util.py
+++ dlint/util.py
@@ -9,3 +9,8 @@ def lstartswith(l1, l2):
     if len(l2) > len(l1):
         return False
     return l1[:len(l2)] == l2
+
+def lendswith(l1, l2):
+    if len(l2) > len(l1):
+        return False
+    return l1[len(l1) - len(l2):] == l2
