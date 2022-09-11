2d7923d5174d88a664305c2b71b47739c7cef8a5
clavedeluna <danalitovsky+git@gmail.com>
2022-09-11 16:21:08 -0300

    test unused util.lendswith

--- tests/test_util.py
+++ tests/test_util.py
@@ -6,9 +6,12 @@ import dlint
 
 
 class TestUtil(unittest.TestCase):
-    pass
 
+    def test_lendswith(self):
+        assert not dlint.util.lendswith([1, 2], [1, 2, 3])
+        assert not dlint.util.lendswith([1, 2, 3], [1, 2])
 
+        assert dlint.util.lendswith([1, 2, 3], [2, 3])
 
 
 if __name__ == "__main__":
